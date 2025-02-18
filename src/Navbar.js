d8e611e42f770ac37fa1e2af0d044cf5c1a37038
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-02-18 07:22:48 +0300

    update

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="bar">
+            <h1>khalayi nandwa's blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/Create">New blog</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
